12a97d73dd76f585c1747f1f0659e381fe7779bc
Nesssu <80637080+Nesssu@users.noreply.github.com>
2022-06-30 19:50:47 +0300

    Now the game is playable via 9 number buttons

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,18 +5,21 @@ import makingBoard from './makingBoard';
 import deletingNumbers from './deletingNumbers';
 import {cloneDeep} from 'lodash';
 
-const handleSelect = (id, getLastIndex, setLastIndex, setSelectedId) => {
-    setSelectedId(id);
+const handleSelect = (id, setSelectedId, getSelectedId) => {
     let elemToAdd = document.getElementById(id);
-    let oldIndex = getLastIndex();
+    let oldIndex = getSelectedId();
     let elem = document.createElement('Circle');
     elem.classList.add("circle");
-    if (oldIndex !== null) {
+    if (oldIndex != null) {
         let elemToDelete = document.getElementById(oldIndex);
-        elemToDelete.removeChild(elemToDelete.lastChild);
+        if (elemToDelete != null && elemToDelete.lastChild != null) {
+            if (elemToDelete.lastChild.classList[0] == "circle") {
+                elemToDelete.removeChild(elemToDelete.lastChild);
+            }
+        }
     }
     elemToAdd.appendChild(elem);
-    setLastIndex(id);
+    setSelectedId(id);
 };
 
 const Number = (props) => {
@@ -24,7 +27,7 @@ const Number = (props) => {
     id = parseInt(id);
     if (props.playable) {
         return (
-            <div className="number-container playable" id={id} onClick={() => handleSelect(id, props.getLastlySelectedId, props.setLastlySelectedId, props.setSelectedId)}>
+            <div className="number-container playable" id={id} onClick={() => handleSelect(id, props.setSelectedId, props.getSelectedId)}>
                 {props.value == 0 ? <p className='number-1'></p> : <p className='number-1'>{props.value}</p>}
             </div>
         )
@@ -42,19 +45,19 @@ const Square = (props) => {
     return (
         <div className="square-container vertical-direction">
             <div className="horizontal-direction">
-                <Number value={props.numbers[props.main][props.secondary]} first={props.main} second={props.secondary} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playable={props.playableBoard[props.main][props.secondary]}/>
-                <Number value={props.numbers[props.main][props.secondary + 1]} first={props.main} second={props.secondary + 1} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playable={props.playableBoard[props.main][props.secondary + 1]}/>
-                <Number value={props.numbers[props.main][props.secondary + 2]} first={props.main} second={props.secondary + 2} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playable={props.playableBoard[props.main][props.secondary + 2]}/>
+                <Number value={props.numbers[props.main][props.secondary]} first={props.main} second={props.secondary} playable={props.playableBoard[props.main][props.secondary]} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
+                <Number value={props.numbers[props.main][props.secondary + 1]} first={props.main} second={props.secondary + 1} playable={props.playableBoard[props.main][props.secondary + 1]} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
+                <Number value={props.numbers[props.main][props.secondary + 2]} first={props.main} second={props.secondary + 2} playable={props.playableBoard[props.main][props.secondary + 2]} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
             </div>
             <div className="horizontal-direction">
-                <Number value={props.numbers[props.main + 1][props.secondary]} first={props.main + 1} second={props.secondary} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playable={props.playableBoard[props.main + 1][props.secondary]}/>
-                <Number value={props.numbers[props.main + 1][props.secondary + 1]} first={props.main + 1} second={props.secondary + 1} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playable={props.playableBoard[props.main + 1][props.secondary + 1]}/>
-                <Number value={props.numbers[props.main + 1][props.secondary + 2]} first={props.main + 1} second={props.secondary + 2} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playable={props.playableBoard[props.main + 1][props.secondary + 2]}/>
+                <Number value={props.numbers[props.main + 1][props.secondary]} first={props.main + 1} second={props.secondary}playable={props.playableBoard[props.main + 1][props.secondary]} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
+                <Number value={props.numbers[props.main + 1][props.secondary + 1]} first={props.main + 1} second={props.secondary + 1} playable={props.playableBoard[props.main + 1][props.secondary + 1]} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
+                <Number value={props.numbers[props.main + 1][props.secondary + 2]} first={props.main + 1} second={props.secondary + 2} playable={props.playableBoard[props.main + 1][props.secondary + 2]} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
             </div>
             <div className="horizontal-direction">
-                <Number value={props.numbers[props.main + 2][props.secondary]} first={props.main + 2} second={props.secondary} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playable={props.playableBoard[props.main + 2][props.secondary]}/>
-                <Number value={props.numbers[props.main + 2][props.secondary + 1]} first={props.main + 2} second={props.secondary + 1} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playable={props.playableBoard[props.main + 2][props.secondary + 1]}/>
-                <Number value={props.numbers[props.main + 2][props.secondary + 2]} first={props.main + 2} second={props.secondary + 2} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playable={props.playableBoard[props.main + 2][props.secondary + 2]}/>
+                <Number value={props.numbers[props.main + 2][props.secondary]} first={props.main + 2} second={props.secondary} playable={props.playableBoard[props.main + 2][props.secondary]} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
+                <Number value={props.numbers[props.main + 2][props.secondary + 1]} first={props.main + 2} second={props.secondary + 1} playable={props.playableBoard[props.main + 2][props.secondary + 1]} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
+                <Number value={props.numbers[props.main + 2][props.secondary + 2]} first={props.main + 2} second={props.secondary + 2} playable={props.playableBoard[props.main + 2][props.secondary + 2]} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
             </div>
         </div>
     );
@@ -64,19 +67,19 @@ const Table = (props) => {
     return (
         <div className="table-container vertical-direction">
             <div className="horizontal-direction">
-                <Square main={0} secondary={0} numbers={props.table} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playableBoard={props.playableBoard}/>
-                <Square main={0} secondary={3} numbers={props.table} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playableBoard={props.playableBoard}/>
-                <Square main={0} secondary={6} numbers={props.table} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playableBoard={props.playableBoard}/>
+                <Square main={0} secondary={0} numbers={props.table} playableBoard={props.playableBoard} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
+                <Square main={0} secondary={3} numbers={props.table} playableBoard={props.playableBoard} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
+                <Square main={0} secondary={6} numbers={props.table} playableBoard={props.playableBoard} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
             </div>
             <div className="horizontal-direction">
-                <Square main={3} secondary={0} numbers={props.table} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playableBoard={props.playableBoard}/>
-                <Square main={3} secondary={3} numbers={props.table} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playableBoard={props.playableBoard}/>
-                <Square main={3} secondary={6} numbers={props.table} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playableBoard={props.playableBoard}/>
+                <Square main={3} secondary={0} numbers={props.table} playableBoard={props.playableBoard} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
+                <Square main={3} secondary={3} numbers={props.table} playableBoard={props.playableBoard} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
+                <Square main={3} secondary={6} numbers={props.table} playableBoard={props.playableBoard} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
             </div>
             <div className="horizontal-direction">
-                <Square main={6} secondary={0} numbers={props.table} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playableBoard={props.playableBoard}/>
-                <Square main={6} secondary={3} numbers={props.table} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playableBoard={props.playableBoard}/>
-                <Square main={6} secondary={6} numbers={props.table} getSelectedId={props.getSelectedId} setSelectedId={props.setSelectedId} getLastlySelectedId={props.getLastlySelectedId} setLastlySelectedId={props.setLastlySelectedId} playableBoard={props.playableBoard}/>
+                <Square main={6} secondary={0} numbers={props.table} playableBoard={props.playableBoard} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
+                <Square main={6} secondary={3} numbers={props.table} playableBoard={props.playableBoard} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
+                <Square main={6} secondary={6} numbers={props.table} playableBoard={props.playableBoard} setSelectedId={props.setSelectedId} getSelectedId={props.getSelectedId}/>
             </div>
         </div>
     );
@@ -91,18 +94,12 @@ const Game =() => {
     const [board, setBoard] = React.useState(null);
     const [solvedBoard, setSolvedBoard] = React.useState(null);
     const [playableBoard, setPlayableBoard] = React.useState(null);
-    //const [lastlySelectedId, setLastlySelectedId] = React.useState(null);
-    let lastlySelectedId = null;
-    const setLastlySelectedId = (newValue) => { lastlySelectedId = newValue; }
-    let selectedId = null;
-    const setSelectedId = (newValue) => {selectedId = newValue;}
     const [newGame, setNewGame] = React.useState(0);
     const [update, setUpdate] = React.useState(0);
-
-    console.log(selectedId);
+    const [selectedId, setSelectedId] = React.useState(null);
 
     const getSelectedId = () => { return selectedId; }
-    const getLastlySelectedId = () => { return lastlySelectedId; }
+
     const separateId = () => {
         let combinedID = null;
         if (selectedId != null) {
@@ -113,6 +110,33 @@ const Game =() => {
             }
         }
         return combinedID;
+    }
+
+    const insertIntoBoard = (number) => {
+        let IdToAdd = separateId();
+        const MAIN = 0; const SECONDARY = 1;
+        if (IdToAdd != null) {
+            if (board != null) {
+                board[IdToAdd[MAIN]][IdToAdd[SECONDARY]] = number;
+                setUpdate(!update);
+            }
+        }
+    }
+
+    const startNewGame = () => {
+        if (selectedId != null) {
+            let elemToDelete = document.getElementById(selectedId);
+            if (elemToDelete != null && elemToDelete.lastChild != null) {
+                if (elemToDelete.lastChild.classList[0] == "circle") {
+                    elemToDelete.removeChild(elemToDelete.lastChild);
+                }
+            }
+        }
+        let newSolvedBoard = makingBoard();
+        setSolvedBoard(cloneDeep(newSolvedBoard));
+        let newBoard = deletingNumbers(newSolvedBoard);
+        setBoard(cloneDeep(newBoard));
+        setNewGame(1);
     }
 
     // RUNS ONCE EVERYTIME THE PAGE GETS RELOADED
@@ -151,9 +175,7 @@ const Game =() => {
         }
     }, [newGame]);
 
-    React.useEffect(() => {
-        console.log(selectedId);
-    }, [update]);
+    React.useEffect(() => {/*This comment disables the empty function warning in VS Code*/}, [update]);
 
     if (board != null) {
         localStorage.setItem("gameBoard", JSON.stringify(board));
@@ -165,42 +187,51 @@ const Game =() => {
         localStorage.setItem("playableGameBoard", JSON.stringify(playableBoard));
     }
 
-    window.addEventListener('keydown', (event) => {
-        if (event.key >= 0 && event.key <= 9) {
-            let IdToAdd = separateId();
-            if (IdToAdd != null) {
-                let boardToAdd = cloneDeep(board);
-                boardToAdd[IdToAdd[0]][IdToAdd[1]] = parseInt(event.key);
-                setBoard(cloneDeep(boardToAdd));
-                setUpdate(update + 1);
-            }
-        }
-    });
     if (board != null && playableBoard != null) {
         return (
             <div className="main-container">
-                <div className="menu-area">
+                <div className="starting-area">
                     <h1 className="headline-1">Sudoku</h1>
                     <button className="play-button" onClick={() => startTheGame()}>
                         <p className="text-button">Play</p>
                     </button>
                 </div>
                 <div className="game-area" id="game">
-                    <h1 className="headline-1">Sudoku</h1>
-                    <Table table={board} getSelectedId={getSelectedId} setSelectedId={setSelectedId} getLastlySelectedId={getLastlySelectedId} setLastlySelectedId={setLastlySelectedId} playableBoard={playableBoard}/>
+                    <div className='menu-area'> 
+                        <p>What is Sudoku?</p>
+                        <p>How to play?</p>
+                        <p>Settings</p>
+                    </div>
+                    <div className='board-area'>
+                        <Table table={board} playableBoard={playableBoard} setSelectedId={setSelectedId} getSelectedId={getSelectedId}/>
+                    </div>
                     <div className="button-area">
-                        <button onClick={() => {let newSolvedBoard = makingBoard(); setSolvedBoard(cloneDeep(newSolvedBoard)); let newBoard = deletingNumbers(newSolvedBoard); setBoard(cloneDeep(newBoard)); setNewGame(1)}}>
-                            New Game
-                        </button>
-                        <button onClick={() => {console.log(solvedBoard)}}>
-                            Solved
-                        </button>
-                        <button onClick={() => {console.log(board); console.log(playableBoard);}}>
-                            Unsolved
-                        </button>
-                        <button onClick={() => {setUpdate(update + 1);}}>
-                            Update
-                        </button>
+                        <div className='btn-row-1'>
+                            <button  className='btn-1' onClick={() => {startNewGame();}}>
+                                New Game
+                            </button>
+                        </div>
+                        <div className='btn-row-3'>
+                            <button className='btn-3' onClick={() => {insertIntoBoard(7);}}>7</button>
+                            <button className='btn-3' onClick={() => {insertIntoBoard(8);}}>8</button>
+                            <button className='btn-3' onClick={() => {insertIntoBoard(9);}}>9</button>
+                        </div>
+                        <div className='btn-row-3'>
+                            <button className='btn-3' onClick={() => {insertIntoBoard(4);}}>4</button>
+                            <button className='btn-3' onClick={() => {insertIntoBoard(5);}}>5</button>
+                            <button className='btn-3' onClick={() => {insertIntoBoard(6);}}>6</button>
+                        </div>
+                        <div className='btn-row-3'>
+                            <button className='btn-3' onClick={() => {insertIntoBoard(1);}}>1</button>
+                            <button className='btn-3' onClick={() => {insertIntoBoard(2);}}>2</button>
+                            <button className='btn-3' onClick={() => {insertIntoBoard(3);}}>3</button>
+                        </div>
+                        <div className='btn-row-1'>
+                            <button className='btn-1'>Empty board</button>
+                        </div>
+                        <div className='btn-row-1'>
+                            <button className='btn-1'>Finish</button>
+                        </div>
                     </div>
                 </div>
             </div>
